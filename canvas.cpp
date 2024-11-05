f8dde25a762496fa82d9e8f90b207557496aae43
sdutson <sdutson222@gmail.com>
2024-11-05 13:18:57 -0700

    Added function outlines to canvas class

--- canvas.cpp
+++ canvas.cpp
@@ -12,3 +12,26 @@ Canvas::~Canvas()
 {
     delete ui;
 }
+
+
+void ScribbleArea::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+        lastPoint = event->position().toPoint();
+        scribbling = true;
+    }
+}
+
+void ScribbleArea::mouseMoveEvent(QMouseEvent *event)
+{
+    if ((event->buttons() & Qt::LeftButton) && scribbling)
+        drawLineTo(event->position().toPoint());
+}
+
+void ScribbleArea::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton && scribbling) {
+        drawLineTo(event->position().toPoint());
+        scribbling = false;
+    }
+}
--- canvas.h
+++ canvas.h
@@ -2,6 +2,7 @@
 #define CANVAS_H
 
 #include <QWidget>
+#include <QImage>
 
 namespace Ui {
 class Canvas;
@@ -11,12 +12,22 @@ class Canvas : public QWidget
 {
     Q_OBJECT
 
+    QImage& image;
+    bool scribbling;
+
 public:
     explicit Canvas(QWidget *parent = nullptr);
     ~Canvas();
 
 private:
     Ui::Canvas *ui;
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+
+    void mouseMoveEvent(QMouseEvent *event) override;
+
+    void mouseReleaseEvent(QMouseEvent *event) override;
 };
 
 #endif // CANVAS_H

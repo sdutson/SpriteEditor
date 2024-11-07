65375f496afa6e2a5d81abddd75e08b64a6d551a
alexanderkuettel <alex.kuettel@gmail.com>
2024-11-07 13:42:21 -0700

    update Sprite header with encapsulation, dimensions to QPair, documentation

--- Sprite.h
+++ Sprite.h
@@ -5,6 +5,7 @@
 #include <QImage>
 
 using std::vector;
+using std::string;
 
 class Sprite {
 
@@ -12,18 +13,46 @@ class Sprite {
 public:
     Sprite();
 
+    ///
+    /// \brief addFrame - adds an empty frame to the sprite.
+    ///
     void addFrame();
 
-    void deleteFrame(QImage &frame);
+    ///
+    /// \brief deleteFrame - deletes frame at index from sprite.
+    /// \param index - index of frame to delete.
+    ///
+    void deleteFrame(int index);
 
+    ///
+    /// \brief getFrame - gets the frame at index.
+    /// \param index - location of the frame to fetch.
+    /// \return - the frame at the index in the sprite. nothing if no frame exists at index.
+    ///
     QImage& getFrame(int index);
 
-    QString name;
+    ///
+    /// \brief setName
+    /// \param name
+    ///
+    void setName(const string& name);
+
+    ///
+    /// \brief getName
+    /// \return
+    ///
+    string getName();
+
 private:
 
-    int dimensions = {16}; // TODO: Change this to whatever we want our default dimensions to be. Note. This will have to be able to be changed later on.
+    ///
+    /// \brief dimensions
+    ///
+    QPair<int, int> dimensions = {16, 16}; // TODO: Change this to whatever we want our default dimensions to be. Note. This will have to be able to be changed later on.
 
     vector<QImage> frames;
+
+    QString name;
 
 };
 
